9c3d7415c370d07c4345e887b8e425d76bf39639
Malachy Kennedy <malachykennedy1@gmail.com>
2025-08-01 21:43:28 -0700

    uktjyhrfdf

--- gmtk25/Assets/_Scripts/DiskSegmentManager.cs
+++ gmtk25/Assets/_Scripts/DiskSegmentManager.cs
@@ -14,26 +14,7 @@ public class DiskSegmentManager : MonoBehaviour
     private void OnEnable()
     {
         segments = new Segment[SEGMENT_COUNT];
-
-        //for (int i = 0; i < SEGMENT_COUNT; ++i)
-        //{
-        //    // i want the angle to be at [360f * i / SEGMENT_COUNT]
-        //    segments[i] = (Instantiate(baseSegmentPrefab, transform.position, Quaternion.Euler(90f, 360f * i / SEGMENT_COUNT, 0f), transform)).GetComponent<Segment>();
-        //}
-
-        // offscreen instantiation to get bounds
-        GameObject temp = Instantiate(baseSegmentPrefab);
-        float height = temp.GetComponent<Renderer>()?.bounds.size.y ?? 1f;
-        Destroy(temp);
-
-        for (int i = 0; i < SEGMENT_COUNT; ++i)
-        {
-            Quaternion rotation = Quaternion.Euler(90f, 360f * i / SEGMENT_COUNT, 0f);            
-            Vector3 position = transform.position - new Vector3(0f, height, 0f);
-
-            segments[i] = Instantiate(baseSegmentPrefab, position, rotation, transform).GetComponent<Segment>();
-        }
-
+        SpawnAndInitializeSegments();
         currentSegment = SEGMENT_COUNT - 1;
     }
 
@@ -44,8 +25,26 @@ public class DiskSegmentManager : MonoBehaviour
         if (ShouldResetCurrentSegment)
         {
             Debug.Log($"CHANGE UR MESH @ SEGMENT #{currentSegment}: {segments[currentSegment]}");
-            currentSegment = (currentSegment - 1) % SEGMENT_COUNT;
+            currentSegment = (currentSegment - 1 + SEGMENT_COUNT) % SEGMENT_COUNT;
             //segments[currentSegment].ChangeMeshTo(); 
+        }
+    }
+
+    private void SpawnAndInitializeSegments()
+    {
+        // temp spawn object to get y displacement
+        // ensure that top of base plane is y = 0
+        GameObject temp = Instantiate(baseSegmentPrefab);
+        float height = temp.GetComponent<Renderer>()?.bounds.max.y ?? 1f;
+        Destroy(temp);
+
+
+        for (int i = 0; i < SEGMENT_COUNT; ++i)
+        {
+            Quaternion rotation = Quaternion.Euler(90f, 360f * i / SEGMENT_COUNT, 0f);
+            Vector3 position = transform.position - new Vector3(0f, height, 0f);
+
+            segments[i] = Instantiate(baseSegmentPrefab, position, rotation, transform).GetComponent<Segment>();
         }
     }
 }
